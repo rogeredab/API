ed7c183947b505605595ecb24c110c6aca6daa0e
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-10-31 15:01:28 -0300

    Ajustado função db para tratativas de erro

--- conn.py
+++ conn.py
@@ -1,10 +1,13 @@
 import pyodbc
 
-
 def db():
-    conn = pyodbc.connect(Driver='{SQL Server}',
-                          Server='localhost',
-                          Database='12001',
-                          Trusted_Connection='yes')
-    cursor = conn.cursor()
-    return cursor
+    try:
+        connection = pyodbc.connect(Driver='{SQL Server}',
+                                    Server='localhost',
+                                    Database='12001',
+                                    Trusted_Connection='yes')
+        print("Conexão bem-sucedida ao banco de dados.")
+        return connection
+    except pyodbc.Error as e:
+        print("Erro ao conectar ao banco de dados:", e)
+        return None

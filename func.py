b75793db07ca88663a14dfbb04d93f7c29e084eb
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-11-01 16:43:06 -0300

    ADICIONADO O PROCESSO DA DATA_LIST QUE TRANSFORMA OS DADOS EM DICT E RETORNA, O QUE EVITA ERROS NO PROCESSO DE TRANSFORMAR EM JSON

--- func.py
+++ func.py
@@ -1,12 +1,14 @@
 from conn import db
 
 
-def cursor(sql):
+def execute_sql(sql):
     conn = db()
-    cursor = conn.cursor()
-    cursor.execute(sql)
-    dados = cursor.fetchall()
-    conn.close()
-    return dados
+    dados = []
+    cur = conn.cursor()
+    cur.execute(sql)
+    dados = cur.fetchall()
+    data_list = [dict(zip([column[0] for column in cur.description], row)) for row in dados]
+    return data_list
+
 
 

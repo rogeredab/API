f43821bcfe38d385931871145ce08c641b8f0b20
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-11-01 17:48:14 -0300

    Tratativas para função get

--- rotas.py
+++ rotas.py
@@ -7,10 +7,14 @@ api = Flask(__name__)
 
 @api.route('/API/TodasRequisicoes', methods=['GET'])
 def getTodasReq():
-    sql = "SELECT * FROM ALMOXARIFADO_REQUISICAO"
-    dados = execute_sql(sql)
-    json = jsonify(dados)
-    return json
+    try:
+        sql = "SELECT * FROM ALMOXARIFADO_REQUISICAO"
+        dados = execute_sql(sql)
+        if not dados:
+            abort(404)
+        return jsonify(dados), 200
+    except Exception as e:
+        abort(500)
 
 if __name__ == '__main__':
     api.run(debug=True)

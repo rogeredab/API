471694017165fb647f485be370ba2c6023c450bf
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-11-01 18:08:18 -0300

    rapido

--- rotas.py
+++ rotas.py
@@ -16,5 +16,18 @@ def getTodasReq():
     except Exception as e:
         abort(500)
 
+
+@api.route('/API/itensreq/<req_id>/<req_emp>', methods=['GET'])
+def getItensReq(req_id, req_emp):
+    try:
+        sql = "SELECT * FROM ALMOXARIFCADO_REQUISICAO_ITENS WHERE ARI_ARE_ID {req_id} and ari_emp_codigo = {req_emp}"
+        dados = execute_sql(sql)
+        if not dados:
+            abort(404)
+        return jsonify(dados), 200
+    except Exception as e:
+        abort(500)
+
+
 if __name__ == '__main__':
     api.run(debug=True)

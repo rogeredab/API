723996a6fee070cd90c60e4badb015ae03099afb
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-11-01 16:43:32 -0300

    Ajustado estrutura para retornar o json, testado e deu certo.

--- rotas.py
+++ rotas.py
@@ -1,5 +1,6 @@
 from flask import Flask, abort, jsonify
-from func import cursor
+from func import execute_sql
+import json
 
 api = Flask(__name__)
 
@@ -7,10 +8,9 @@ api = Flask(__name__)
 @api.route('/API/TodasRequisicoes', methods=['GET'])
 def getTodasReq():
     sql = "SELECT * FROM ALMOXARIFADO_REQUISICAO"
-    dados = cursor(sql)
+    dados = execute_sql(sql)
     json = jsonify(dados)
     return json
-
 
 if __name__ == '__main__':
     api.run(debug=True)

6f56a8eb0db0e73d1c02c94a06c0d813366b786d
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-11-03 16:09:00 -0300

    NOVO GET PARA REQUISIÇÃO ESPECIFICA

--- rotas.py
+++ rotas.py
@@ -1,6 +1,5 @@
 from flask import Flask, abort, jsonify
 from func import execute_sql
-import json
 
 api = Flask(__name__)
 
@@ -20,13 +19,27 @@ def getTodasReq():
 @api.route('/API/itensreq/<req_id>/<req_emp>', methods=['GET'])
 def getItensReq(req_id, req_emp):
     try:
-        sql = "SELECT * FROM ALMOXARIFADO_REQUISICAO_ITENS WHERE ARI_ARE_ID = {} and ari_emp_codigo = {}".format(req_id, req_emp)
+        sql = "SELECT * FROM ALMOXARIFADO_REQUISICAO_ITENS WHERE ARI_ARE_ID = {} and ari_emp_codigo = {}".format(req_id,
+                                                                                                                 req_emp)
         dados = execute_sql(sql)
         if not dados:
             abort(404)
         return jsonify(dados), 200
     except Exception as e:
         print("Erro: ", e)
+        abort(500)
+
+
+@api.route('/API/req/<req_id>/<req_emp>', methods=['GET'])
+def getReqEsp(req_id, req_emp):
+    try:
+        sql = "SELECT * FROM ALMOXARIFADO_REQUISICAO WHERE ARE_ID = {} and are_emp_codigo = {}".format(req_id, req_emp)
+        dados = execute_sql(sql)
+        if not dados:
+            abort(404)
+        return jsonify(dados), 200
+    except Exception as e:
+        print("Erro:", e)
         abort(500)
 
 

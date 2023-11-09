524b9243f5dcd8e625b95b368848866eb4e9791d
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-11-09 15:15:34 -0300

    3 DELETES CRIADOS, DELETE PARA REQUISICAO ESPECIFICA, DELETE PARA TODOS ITEMS DE UMA REQUISICAO, DELETE PARA 1 ITEM DE UMA REQUISICAO PELA ORDEM ITEM. TAMBÉM NOTADO QUE A REQRETIRADA NÃO CONSTA NO POSTMAN, AJUSTAR SQL E TESTAR

--- rotas.py
+++ rotas.py
@@ -1,5 +1,5 @@
 from flask import Flask, abort, jsonify
-from func import execute_sql
+from func import execute_sql, delete_sql
 
 api = Flask(__name__)
 
@@ -50,7 +50,7 @@ def getReqEsp(req_id, req_emp):
 @api.route('/API/reqretirada/<reqi_id>/<req_emp>', methods=['GET'])
 def getReqRet(reqi_id, req_emp):
     try:
-        sql = "SELECT * FROM ALMOXARIFADO_REQUISICAO_RETIRADA WHERE ARR_ARI_ID = {} AND ARR_EMP_CODIGO = {}".format(
+        sql = "SELECT * FROM ALMOXARIFADO_REQUISICAO_RETIRADA WHERE ARR_ARE_ID = {} AND ARR_EMP_CODIGO = {}".format(
             reqi_id, req_emp)
         dados = execute_sql(sql)
         if not dados:
@@ -62,12 +62,36 @@ def getReqRet(reqi_id, req_emp):
         abort(500)
 
 
-@api.route('/API/reqdel/<reqi_id>/<req_emp>', methods=['DELETE'])
+@api.route('/API/reqdel/<req_id>/<req_emp>', methods=['DELETE'])
 def delReq(req_id, req_emp):
     try:
         sql = "DELETE FROM ALMOXARIFADO_REQUISICAO WHERE ARE_ID = {} and are_emp_codigo = {}".format(req_id, req_emp)
-        execute_sql(sql)
+        delete_sql(sql)
         return jsonify({"message": "Solicitação de retirada excluída com sucesso!"}), 200
+
+    except Exception as e:
+        print("Erro", e)
+        abort(500)
+
+
+@api.route('/API/reqitensdel/<req_id>/<req_emp>', methods=['DELETE'])
+def delReqItens(req_id, req_emp):
+    try:
+        sql = "DELETE FROM ALMOXARIFADO_REQUISICAO_ITENS WHERE ARI_ARE_ID = {} and ari_emp_codigo = {}".format(req_id, req_emp)
+        delete_sql(sql)
+        return jsonify({"message": "Solicitação de retirada excluída com sucesso!"}), 200
+
+    except Exception as e:
+        print("Erro", e)
+        abort(500)
+
+
+@api.route('/API/reqitemdel/<req_id>/<req_emp>/<reqi_ni>', methods=['DELETE'])
+def delReqItem(req_id, req_emp, reqi_ni):
+    try:
+        sql = "DELETE FROM ALMOXARIFADO_REQUISICAO_ITENS WHERE ARI_ARE_ID = {} and ari_emp_codigo = {} and ari_ni = {}".format(req_id,req_emp,reqi_ni)
+        delete_sql(sql)
+        return jsonify({"message": "Item solicitado retirado com sucesso!"}), 200
 
     except Exception as e:
         print("Erro", e)

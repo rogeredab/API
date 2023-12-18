e5c510347664fe1e56378e64158d40e1b973a40f
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-12-18 11:48:03 -0300

    ALTERADO METODO DE CONEXÃO COM O BANCO PARA UTILIZAR UM CONFIG.INI

--- conn.py
+++ conn.py
@@ -1,16 +1,41 @@
 import pyodbc
+import configparser
+import os
+
+
+def criar_config():
+    config = configparser.ConfigParser()
+    config['BANCO_DE_DADOS'] = {
+        'Driver': '{SQL Server}',
+        'Server': 'localhost',
+        'Database': '12001',
+        'user': 'sa',
+        'password': 'omni@50ftp4r'
+    }
+
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+
+def ler_config():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['BANCO_DE_DADOS']
 
 
 def db():
+    if not os.path.exists('config.ini'):
+        criar_config()
+
     try:
-        connectionAuth = pyodbc.connect(Driver='{SQL Server}',
-                                        Server='localhost',
-                                        Database='12001',
-                                        user='sa',
-                                        password='roger123')
+        config_banco = ler_config()
+        connectionAuth = pyodbc.connect(Driver=config_banco['Driver'],
+                                        Server=config_banco['Server'],
+                                        Database=config_banco['Database'],
+                                        user=config_banco['user'],
+                                        password=config_banco['password'])
         print('Conexão bem-sucedida com autenticação de usuário/senha')
         return connectionAuth
     except pyodbc.Error as e:
         print('Erro de autenticação de usuário/senha:', e)
         return None
-

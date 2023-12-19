d1d3f5456f145bbc8448d405eaf070ac8d61eeda
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-12-19 11:35:57 -0300

    COMMIT REFATORACAO conn.py

--- conn.py
+++ conn.py
@@ -1,41 +1,22 @@
-import pyodbc
+from sqlalchemy import create_engine
 import configparser
-import os
 
 
-def criar_config():
-    config = configparser.ConfigParser()
-    config['BANCO_DE_DADOS'] = {
-        'Driver': '{SQL Server}',
-        'Server': 'localhost',
-        'Database': '12001',
-        'user': 'sa',
-        'password': 'omni@50ftp4r'
-    }
-
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
-
-
-def ler_config():
+def criar_conexao():
     config = configparser.ConfigParser()
     config.read('config.ini')
-    return config['BANCO_DE_DADOS']
+
+    driver = config['BANCO_DE_DADOS']['driver']
+    server = config['BANCO_DE_DADOS']['server']
+    database = config['BANCO_DE_DADOS']['database']
+    user = config['BANCO_DE_DADOS']['user']
+    password = config['BANCO_DE_DADOS']['password']
+
+    connection_string = f"{driver}://{user}:{password}@{server}/{database}"
+
+    engine = create_engine(connection_string)
+    connection = engine.connect()
+
+    return connection
 
 
-def db():
-    if not os.path.exists('config.ini'):
-        criar_config()
-
-    try:
-        config_banco = ler_config()
-        connectionAuth = pyodbc.connect(Driver=config_banco['Driver'],
-                                        Server=config_banco['Server'],
-                                        Database=config_banco['Database'],
-                                        user=config_banco['user'],
-                                        password=config_banco['password'])
-        print('Conexão bem-sucedida com autenticação de usuário/senha')
-        return connectionAuth
-    except pyodbc.Error as e:
-        print('Erro de autenticação de usuário/senha:', e)
-        return None

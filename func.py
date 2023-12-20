9384fe1f392bfc0126da699a34c9d55fde1ea2c0
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-12-20 17:32:00 -0300

    AJUSTADO FUNÇÃO QUE SÓ CONSEGUIA PROCURAR NA MODEL ALMOXARIFADO_REQUISICAO, TESTADO E FUNCIONANDO, TALVEZ TENHA QUE FAZER AMIS UM CASE

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+import models
 from conn import db
 
 
@@ -22,9 +23,12 @@ def select_filter(tabela, filtro):
     session = db()
     lista = []
     try:
-        dados = session.query(tabela).filter(tabela.ARE_ID == filtro[0], tabela.ARE_EMP_CODIGO == filtro[1])
-        lista = [row.as_dict() for row in dados]
-
+        if tabela == models.Almoxarifado_requisicao:
+            dados = session.query(tabela).filter(tabela.ARE_ID == filtro[0], tabela.ARE_EMP_CODIGO == filtro[1])
+            lista = [row.as_dict() for row in dados]
+        elif tabela == models.Almoxarifado_requisicao_itens:
+            dados = session.query(tabela).filter(tabela.ARI_ARE_ID == filtro[0], tabela.ARI_EMP_CODIGO == filtro[1])
+            lista = [row.as_dict() for row in dados]
     except Exception as e:
         print(f"Erro ao executar a consulta: {e}")
 

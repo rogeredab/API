ee732078f35fee458b94b0d0f9e3eab17c368df5
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-12-21 16:14:19 -0300

    AJUSTADO SELECT_FILTER PARA O PROCESSO DE RETIRADA QUE USA UM .JOIN()

--- func.py
+++ func.py
@@ -29,6 +29,16 @@ def select_filter(tabela, filtro):
         elif tabela == models.Almoxarifado_requisicao_itens:
             dados = session.query(tabela).filter(tabela.ARI_ARE_ID == filtro[0], tabela.ARI_EMP_CODIGO == filtro[1])
             lista = [row.as_dict() for row in dados]
+        elif tabela == models.Almoxarifado_requisicao_retirada:
+            dados = (
+                session.query(tabela, models.Almoxarifado_requisicao_itens)
+                .join(models.Almoxarifado_requisicao_itens,
+                      models.Almoxarifado_requisicao_itens.ARI_ID == tabela.ARR_ARI_ID)
+                .filter(models.Almoxarifado_requisicao_itens.ARI_ARE_ID == filtro[0],
+                        tabela.ARR_EMP_CODIGO == filtro[1])
+                .all()
+            )
+            lista = [row[0].as_dict() for row in dados]
     except Exception as e:
         print(f"Erro ao executar a consulta: {e}")
 

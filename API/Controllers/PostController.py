0d543a1e3ffe5fdb651ac4e4cd5c9a8e739d23f5
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-12-27 17:49:22 -0300

    CONTINUAR DEPOIS

--- API/Controllers/PostController.py
+++ API/Controllers/PostController.py
@@ -1,5 +1,9 @@
+from flask import jsonify
+
 from API.Models import models
+from API.Services.DateTime import get_current_date
 from Config.conn import db
+import json
 
 
 class PostController:
@@ -14,6 +18,14 @@ class PostController:
     def insert_req(self, tabela, dados):
         try:
             if tabela == models.Almoxarifado_requisicao:
-
+                last = self.session.query(tabela.ARE_ID).order_by(tabela.ARE_ID.desc()).first()
+                lastmais = last[0] + 1
+                dadosins = dados
+                dadosins['ARE_ID'] = lastmais
+                dadosins['ARE_DATA_RETIRADA'] = get_current_date()
+                dadosins['ARE_DATA_SOLICITACAO'] = get_current_date()
+                dadosins['ARE_DATAINC'] = get_current_date()
+                data = jsonify(dadosins)
+                print(data)
         except Exception as e:
             print(e)

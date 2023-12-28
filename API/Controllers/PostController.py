883f7fe2467ba817d2d4d1f078ed712ce3310f6a
rogeredab <140016930+rogeredab@users.noreply.github.com>
2023-12-28 11:56:37 -0300

    AJUSTADO O INSERT E AGORA O RESTO DEVE FICAR MAIS FÁCIL, INSERIDO DEMAIS INFOS, VALIDAÇÃO SÓ PELO ARE_ID

--- API/Controllers/PostController.py
+++ API/Controllers/PostController.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 
 from API.Models import models
+from API.Models.models import Almoxarifado_requisicao
 from API.Services.DateTime import get_current_date
 from Config.conn import db
 import json
@@ -16,16 +17,22 @@ class PostController:
 
     @staticmethod
     def insert_req(self, tabela, dados):
+        retorno = []
         try:
             if tabela == models.Almoxarifado_requisicao:
                 last = self.session.query(tabela.ARE_ID).order_by(tabela.ARE_ID.desc()).first()
                 lastmais = last[0] + 1
-                dadosins = dados
-                dadosins['ARE_ID'] = lastmais
-                dadosins['ARE_DATA_RETIRADA'] = get_current_date()
-                dadosins['ARE_DATA_SOLICITACAO'] = get_current_date()
-                dadosins['ARE_DATAINC'] = get_current_date()
-                data = jsonify(dadosins)
-                print(data)
+                dados["ARE_ID"] = lastmais
+                dados["ARE_DATA_SOLICITACAO"] = get_current_date()
+                dados["ARE_DATAINC"] = get_current_date()
+                nova_requisicao = Almoxarifado_requisicao(**{k.upper(): v for k, v in dados.items()})
+                self.session.add(nova_requisicao)
+                self.session.commit()
+                conf = self.session.query(tabela.ARE_ID).filter(tabela.ARE_ID == lastmais).all()
+                if not conf:
+                    retorno = jsonify({"message": "Não foi possivel realizar a inserção"}), 500
+                else:
+                    retorno = jsonify({"message": "Sucesso ao realizar a inserção"}), 200
+            return retorno
         except Exception as e:
             print(e)

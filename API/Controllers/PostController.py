56eaa6bc15f652e8fc3e19967bfe0f5478be18a6
rogeredab <140016930+rogeredab@users.noreply.github.com>
2024-01-03 17:44:28 -0300

    AJUSTADO NOME DA FUNC

--- API/Controllers/PostController.py
+++ API/Controllers/PostController.py
@@ -1,10 +1,10 @@
 from flask import jsonify
+from sqlalchemy import desc
 
 from API.Models import models
-from API.Models.models import Almoxarifado_requisicao
+from API.Models.models import Almoxarifado_requisicao, Almoxarifado_requisicao_itens
 from API.Services.DateTime import get_current_date
 from Config.conn import db
-import json
 
 
 class PostController:
@@ -34,5 +34,28 @@ class PostController:
                 else:
                     retorno = jsonify({"message": "Sucesso ao realizar a inserção"}), 200
             return retorno
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def insert_req_item(self, tabela, dados, filtro):
+        retorno = []
+        try:
+            if tabela == models.Almoxarifado_requisicao_itens:
+                check1 = self.session.query(Almoxarifado_requisicao.ARE_ID).filter(
+                    Almoxarifado_requisicao.ARE_ID == filtro[0], Almoxarifado_requisicao == filtro[1]).all()
+                if not check1:
+                    retorno = jsonify(
+                        {"message": "Requisição inexistente ou código da empresa informado incorretamente"}), 404
+            checkstatus = self.session.query(Almoxarifado_requisicao.ARE_STATUS).filter(
+                Almoxarifado_requisicao.ARE_ID == filtro[0], Almoxarifado_requisicao == filtro[1]).all()
+            status = int(checkstatus[0])
+            if status == 3:
+                return jsonify({"message": "Requisição se encontra cancelada"}), 500
+            elif status == 2:
+                return jsonify({"message": "Requisição já foi retirada"}), 500
+            checkarid = self.session.query(tabela.ARI_ID).filter(tabela.ARI_EMP_CODIGO == filtro[1]).order_by(tabela.ARI_ID.desc()).first()
+            ari_idplus = int(checkarid[0]) + 1
+
         except Exception as e:
             print(e)

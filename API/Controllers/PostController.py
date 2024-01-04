e9d01c2761dd37c6899937461b9bb6e83fd769fb
rogeredab <140016930+rogeredab@users.noreply.github.com>
2024-01-04 11:04:38 -0300

    AJUSTADO TODA LÓGICA E CHECAGEM, PRÓXIMO PASSO SERA O PUT E O PATCH DAS TABELAS DE REQUISIÇÕES E ITENS REQUISIÇÕES. FUNCIONANDO E TESTADO COM O POSTMAN.

--- API/Controllers/PostController.py
+++ API/Controllers/PostController.py
@@ -17,7 +17,6 @@ class PostController:
 
     @staticmethod
     def insert_req(self, tabela, dados):
-        retorno = []
         try:
             if tabela == models.Almoxarifado_requisicao:
                 last = self.session.query(tabela.ARE_ID).order_by(tabela.ARE_ID.desc()).first()
@@ -30,32 +29,64 @@ class PostController:
                 self.session.commit()
                 conf = self.session.query(tabela.ARE_ID).filter(tabela.ARE_ID == lastmais).all()
                 if not conf:
-                    retorno = jsonify({"message": "Não foi possivel realizar a inserção"}), 500
+                    return jsonify({"message": "Não foi possivel realizar a inserção"}), 500
                 else:
-                    retorno = jsonify({"message": "Sucesso ao realizar a inserção"}), 200
-            return retorno
+                    return jsonify({"message": "Sucesso ao realizar a inserção"}), 200
+
         except Exception as e:
             print(e)
 
     @staticmethod
     def insert_req_item(self, tabela, dados, filtro):
-        retorno = []
         try:
             if tabela == models.Almoxarifado_requisicao_itens:
-                check1 = self.session.query(Almoxarifado_requisicao.ARE_ID).filter(
-                    Almoxarifado_requisicao.ARE_ID == filtro[0], Almoxarifado_requisicao == filtro[1]).all()
+                check1 = self.session.query(Almoxarifado_requisicao.ARE_ID).filter(Almoxarifado_requisicao.ARE_ID == filtro[0],Almoxarifado_requisicao.ARE_EMP_CODIGO == filtro[1]).all()
                 if not check1:
-                    retorno = jsonify(
-                        {"message": "Requisição inexistente ou código da empresa informado incorretamente"}), 404
-            checkstatus = self.session.query(Almoxarifado_requisicao.ARE_STATUS).filter(
-                Almoxarifado_requisicao.ARE_ID == filtro[0], Almoxarifado_requisicao == filtro[1]).all()
-            status = int(checkstatus[0])
+                    return jsonify({"message": "Requisição inexistente ou código da empresa informado incorretamente"}), 404
+                else:
+                    pass
+            checkstatus = self.session.query(Almoxarifado_requisicao.ARE_STATUS).filter(Almoxarifado_requisicao.ARE_ID == filtro[0], Almoxarifado_requisicao.ARE_EMP_CODIGO == filtro[1]).all()
+            status = checkstatus[0][0]
             if status == 3:
                 return jsonify({"message": "Requisição se encontra cancelada"}), 500
             elif status == 2:
                 return jsonify({"message": "Requisição já foi retirada"}), 500
             checkarid = self.session.query(tabela.ARI_ID).filter(tabela.ARI_EMP_CODIGO == filtro[1]).order_by(tabela.ARI_ID.desc()).first()
-            ari_idplus = int(checkarid[0]) + 1
+            ari_idplus = checkarid[0] + 1
 
+            checkni = self.session.query(tabela.ARI_NI).filter(tabela.ARI_ARE_ID == filtro[0],tabela.ARI_EMP_CODIGO == filtro[1]).order_by( tabela.ARI_NI.desc()).first()
+            nilast = ''
+            if checkni and checkni[0] == '':
+                nilast = 1
+            elif checkni:
+                nilast = checkni[0] + 1
+            else:
+                nilast = 1
+
+            dados['ari_id'] = ari_idplus
+            dados['ari_emp_codigo'] = filtro[1]
+            dados['ari_ni'] = nilast
+            dados['ari_are_id'] = filtro[0]
+            dados['ari_datainc'] = get_current_date()
+
+            item_found = False
+            checkpro = self.session.query(tabela.ARI_PRO_CODIGO).filter(tabela.ARI_ARE_ID == filtro[0],tabela.ARI_EMP_CODIGO == filtro[1]).all()
+            for item in checkpro:
+                print("ITEMS",item[0])
+                print("DADOS",dados['ari_pro_codigo'])
+                if item[0] == dados['ari_pro_codigo']:
+                    item_found = True
+                    break
+            if item_found:
+                return jsonify({"Message": "Item já inserido na requisição, utilize uma rota de put ou patch"}), 500
+
+            nova_requisicao = Almoxarifado_requisicao_itens(**{k.upper(): v for k, v in dados.items()})
+            self.session.add(nova_requisicao)
+            self.session.commit()
+            conf = self.session.query(tabela.ARI_NI).filter(tabela.ARI_NI == nilast, tabela.ARI_ARE_ID == filtro[0], tabela.ARI_EMP_CODIGO == filtro[1]).all()
+            if not conf:
+                return jsonify({"message": "Não foi possivel realizar a inserção"}), 500
+            else:
+                return jsonify({"message": "Sucesso ao realizar a inserção"}), 200
         except Exception as e:
             print(e)

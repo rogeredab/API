e37bac52d313449966553aca52b06bfd7e1eea81
rogeredab <140016930+rogeredab@users.noreply.github.com>
2024-01-19 14:12:05 -0300

    Adicionado os controllers e rota ao main

--- API/root/main.py
+++ API/root/main.py
@@ -3,6 +3,7 @@ from API.Routes.DelRoutes import api as del_api
 from API.Routes.GetRoutes import api as get_api
 from API.Routes.PostRoutes import api as post_api
 from API.Routes.PutRoutes import api as put_api
+from API.Routes.PatchRoutes import api as patch_api
 
 app = Flask(__name__)
 
@@ -10,6 +11,6 @@ app.register_blueprint(del_api)
 app.register_blueprint(get_api)
 app.register_blueprint(post_api)
 app.register_blueprint(put_api)
-
+app.register_blueprint(patch_api)
 if __name__ == '__main__':
     app.run(debug=True)

22a23357143f146b8a552d6bf2693f8de98947f8
rogeredab <140016930+rogeredab@users.noreply.github.com>
2024-01-19 14:13:04 -0300

    CRIADO O CONTROLLER DO PATCH

---
+++ API/Controllers/PatchController.py
@@ -0,0 +1,28 @@
+from flask import jsonify
+from API.Models import models
+from API.Services.DateTime import get_current_date
+from Config.conn import db
+
+
+class PatchController:
+
+    def __init__(self):
+        self.session = db()
+
+    def _close_session(self):
+        self.session.close()
+
+    @staticmethod
+    def patch_req(self, tabela, dados, filtro):
+        try:
+            if tabela == models.Almoxarifado_requisicao:
+                checkexiste = self.session.query(models.Almoxarifado_requisicao).filter_by(ARE_ID=filtro[0],
+                                                                                           ARE_EMP_CODIGO=filtro[
+                                                                                               1]).first()
+
+                if not checkexiste:
+                    return jsonify({"message": "Requisição inexistente ou parametros inválidos"}), 404
+                else:
+                    print(dados)
+        except Exception as e:
+            print(e)
